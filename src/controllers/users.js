36ef2320847cee7849cda6da590af9e83f2f0d06
DaJuan Harris <dajuancancode@gmail.com>
2019-07-13 16:29:25 -0400

    Added ability to upload avatars

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,4 @@
+const sharp = require('sharp')
 const User = require('../models/userModel')
 
 const createUser = async (req, res) => {
@@ -71,4 +72,33 @@ const updateUser = async (req,res) => {
     res.status(400).send()
   }
 }
-module.exports = { createUser, loginUser, logoutUser, viewUser, deleteUser, updateUser }
+
+
+const uploadAvatar = async (req, res) => {
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer()
+
+  req.user.avatar = buffer
+  await req.user.save()
+  res.send()
+}
+
+const deleteAvatar = async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send()
+}
+
+const fetchAvatar = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    
+    if (!user || !user.avatar) {
+      throw new Error()
+    }
+    res.set('Content-Type', 'image/png')
+    res.send(user.avatar)
+  } catch (e) {
+    res.status(404).send()
+  }
+}
+module.exports = { createUser, loginUser, logoutUser, viewUser, deleteUser, updateUser, uploadAvatar, deleteAvatar, fetchAvatar }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,23 @@
 const { Router } = require('express')
+const multer = require('multer')
 const authMiddleware = require('../middleware/authentication')
 
-const { createUser, loginUser, logoutUser, viewUser, deleteUser, updateUser } = require('../controllers/users')
+const { createUser, loginUser, logoutUser, viewUser, deleteUser, updateUser, uploadAvatar, deleteAvatar, fetchAvatar} = require('../controllers/users')
 
 const apiRouter = Router()
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
+      return cb(new Error('Please upload a image'))
+    }
+    
+    cb(undefined, true)
+  }
+})
 
 apiRouter.post( '/signup', createUser )
 apiRouter.post( '/login', loginUser )
@@ -13,6 +27,13 @@ apiRouter.get('/profile/:id', viewUser)
 apiRouter.patch('/profile', authMiddleware, updateUser)
 
 apiRouter.delete('/delete', authMiddleware, deleteUser)
+
+apiRouter.post('/me/avatar', authMiddleware, upload.single('avatar'), uploadAvatar, (error, req, res, next) => {
+  res.status(400).send({error: error.message})
+})
+
+apiRouter.delete('/me/avatar', authMiddleware, deleteAvatar)
+apiRouter.get('/:id/avatar', fetchAvatar)
 
 
 
